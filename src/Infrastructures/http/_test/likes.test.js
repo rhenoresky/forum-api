5f573f1e45046cef82cfcd1115916664d7bf99bd
rhenoresky <rheno2002@gmail.com>
2023-11-27 18:02:54 +0700

    add likes infrastructures

---
+++ src/Infrastructures/http/_test/likes.test.js
@@ -0,0 +1,93 @@
+const pool = require('../../database/postgres/pool');
+const ThreadsTableTestHelper = require('../../../../tests/ThreadsTableTestHelper');
+const UsersTableTestHelper = require('../../../../tests/UsersTableTestHelper');
+const CommentsTableTestHelper = require('../../../../tests/CommentsTableTestHelper');
+const LikesTableTestHelper = require('../../../../tests/LikesTableTestHelper');
+const TokenHelper = require('../../../../tests/TokenHelper');
+const container = require('../../container');
+const createServer = require('../../http/createServer');
+
+describe('/threads/{threadId}/comments/{commentId}/likes', () => {
+  afterEach(async () => {
+    await ThreadsTableTestHelper.cleanTable();
+    await UsersTableTestHelper.cleanTable();
+    await CommentsTableTestHelper.cleanTable();
+    await LikesTableTestHelper.cleanTable();
+  });
+
+  afterAll(async () => {
+    await pool.end();
+  });
+
+  describe('when PUT /threads/{threadId}/comments/{commentId}/likes', () => {
+    it('should response 401 when missing authentication', async () => {
+      const server = await createServer(container);
+      const response = await server.inject({
+        url: '/',
+        method: 'PUT',
+      });
+
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(401);
+      expect(responseJson.error).toEqual('Unauthorized');
+    });
+
+    it('should respond to 404 when comment is not found', async () => {
+      const token = await TokenHelper.createAccessToken(username = 'robin', id = 'user-123');
+      const server = await createServer(container);
+      const response = await server.inject({
+        url: '/',
+        method: 'PUT',
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      });
+
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(404);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual('Comment tidak ditemukan');
+    });
+
+    it('should response 200 and persisted add likes', async () => {
+      await UsersTableTestHelper.addUser({});
+      await ThreadsTableTestHelper.addThread({});
+      const token = await TokenHelper.createAccessToken(username = 'robin', id = 'user-456');
+      await CommentsTableTestHelper.addComment({});
+
+      const server = await createServer(container);
+      const response = await server.inject({
+        url: '/',
+        method: 'PUT',
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      });
+
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(200);
+      expect(responseJson.status).toEqual('success');
+    });
+
+    it('should response 200 and persisted delete likes', async () => {
+      await UsersTableTestHelper.addUser({});
+      await ThreadsTableTestHelper.addThread({});
+      const token = await TokenHelper.createAccessToken(username = 'robin', id = 'user-456');
+      await CommentsTableTestHelper.addComment({});
+      await LikesTableTestHelper.addLike({owner: 'user-456'});
+
+      const server = await createServer(container);
+      const response = await server.inject({
+        url: '/',
+        method: 'PUT',
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      });
+
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(200);
+      expect(responseJson.status).toEqual('success');
+    });
+  });
+});
